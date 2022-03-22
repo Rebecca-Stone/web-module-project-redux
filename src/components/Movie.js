cc3394d2dfeb9ab1cab54c1e824f379673830c0f
Rebecca-Stone <primstone16@gmail.com>
2022-03-22 18:14:02 -0500

    finished Connecting the Delete and Add Movie actions

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
+import { deleteMovie } from './../actions/movieActions';
 
 const Movie = (props) => {
   const { id } = useParams();
   const { push } = useHistory();
 
-  const { movies } = props;
+  const { movies, deleteMovie } = props;
   const movie = movies.find((movie) => movie.id === Number(id));
+
+  const deleteClick = () => {
+    deleteMovie(movie.id);
+    push('/movies');
+  };
 
   return (
     <div className="modal-page col">
@@ -49,11 +55,14 @@ const Movie = (props) => {
 
               <section>
                 <span className="m-2 btn btn-dark">Favorite</span>
-                <span className="delete">
+                <span className="delete"
+                onClick={deleteClick}
+                >
                   <input
                     type="button"
                     className="m-2 btn btn-danger"
                     value="Delete"
+                    
                   />
                 </span>
               </section>
@@ -71,4 +80,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Movie);
+export default connect(mapStateToProps, { deleteMovie })(Movie);
